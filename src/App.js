9c65a2c0b736d577d3091539453324146b72fe34
Jeongmin Cho <jojm0829@gmail.com>
2023-02-10 23:46:57 +0900

    #14.0 Getting Ready for the Router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie.js";
-import "./App.css";
+import Home from "./routes/Home.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-    console.log(this.state.movies);
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                year={movie.year}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
